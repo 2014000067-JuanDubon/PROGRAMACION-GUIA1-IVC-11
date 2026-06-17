79402ab1ab578d438878fe08432901e6c9f2c522
2014000067-JuanDubon <j.dubon.5adb@gmail.com>
2026-06-16 20:46:47 -0600

    Add files via upload

---
+++ Cantidadeje1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] edadesPersonas = new int[7];
+            int cantidadMayoresEdad = 0;
+
+            for (int indice = 0; indice < 7; indice++)
+            {
+                Console.Write("Ingrese la edad de la persona " + (indice + 1) + ": ");
+                edadesPersonas[indice] = int.Parse(Console.ReadLine());
+
+                if (edadesPersonas[indice] >= 18)
+                {
+                    cantidadMayoresEdad++;
+                }
+            }
+
+            Console.WriteLine("Cantidad de personas mayores de edad: " + cantidadMayoresEdad);
+            Console.ReadKey();
+        }
+    }
+}
